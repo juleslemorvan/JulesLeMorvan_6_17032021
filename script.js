8f9db320def526593f170032966989bc5c30552f
Le Morvan <jules.lemorvan01@gmail.com>
2021-04-04 22:08:54 +0200

    catch json file

---
+++ script.js
@@ -0,0 +1,15 @@
+// Fetch Data
+
+const data = "./data.json";
+
+// Data from json file
+fetch("/data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
